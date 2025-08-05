670d4bd0184b494857fec57b5e1f46708ba1feb7
llfung <35137249+llfung@users.noreply.github.com>
2025-08-05 17:28:02 +0000

    Deploying to gh-pages from @ llfung/llfung.github.io@9d33de3623ae34c1c47f6b2b1094429cb62c2b0a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is a brief CV following the [JSON Resume standard](https://jsonresume.org). For the complete version, follow the link to the PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
